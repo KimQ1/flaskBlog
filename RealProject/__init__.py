bd6cf03aac6094cd80d44558233e57a15c007cda
Administrator <1766730816@qq.com>
2022-08-30 15:31:55 +0800

    Initial commit

---
+++ RealProject/__init__.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask
+
+# app = Flask(__name__)
+#
+#
+# @app.route('/')
+# def hello_world():  # put application's code here
+#     # return 'Hello World!'
+#     return "<p> Hello,World2!</P>"
+
+def create_app(test_config=)
